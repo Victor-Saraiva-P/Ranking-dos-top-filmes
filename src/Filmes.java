89acc96b7aca586dc6bc843dccf1bb1407218d4f
Victor Alexandre Saraiva Pimentel <victor_alex2005@hotmail.com>
2024-06-07 00:18:35 -0300

    captura de valores json concluido

--- src/Filmes.java
+++ src/Filmes.java
@@ -3,16 +3,32 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Filmes {
-    private List<String> titles;
+import javax.lang.model.util.SimpleElementVisitor14;
 
+public class Filmes {
+    // PROPRIEDADES
+    private List<String> titles;
+    private List<String> posterPath;
+    private List<String> voteAverage;
+
+    // metodos comuns
     public Filmes(String json) {
-        setTitles(pegarAtributo("nome", json));
+        setTitles(pegarAtributo("title", json));
+        setposterPath(pegarAtributo("poster_path", json));
+        setvoteAverage(pegarAtributo("vote_average", json));
     }
 
-    public List<String> pegarAtributo(String propriedade, String json)
-    {
-        String regex = String.format("\"%s\":\\s*\"([^\"]+)\"", propriedade); // Padrão para palavras
+    public List<String> pegarAtributo(String propriedade, String json) {
+
+        String regex;
+        if (propriedade == "vote_average") // checa se o valor se trata de um vote_average que é tratado diferente por
+                                           // ser um número
+        {
+            regex = String.format("\"%s\":\\s*(\\d+(\\.\\d{1,3})?)", propriedade);
+        } else // outro, seguir normalmente o código
+        {
+            regex = String.format("\"%s\":\\s*\"([^\"]+)\"", propriedade);
+        }
 
         // Compilar a expressão regular em um objeto Pattern
         Pattern pattern = Pattern.compile(regex);
@@ -24,16 +40,43 @@ public class Filmes {
         List<String> listaResultado = new ArrayList<>(); // criar uma lista que vai conter os matchs
 
         while (matcher.find()) {
-            String expressão = matcher.group().replace("\"", ""); // retirar as aspas
-            expressão = expressão.replace(String.format("%s: ", propriedade), "");
-            listaResultado.add(expressão);
+            String expressao;
+
+            if (propriedade == "poster_path") // verificar se trata dum link para colocar o hipertexto antes
+            {
+                expressao = "https://image.tmdb.org/t/p/" + matcher.group();
+            } else {
+                expressao = (String) matcher.group();
+            }
+
+            expressao = expressao.replace("\"", ""); // retirar as aspas
+            expressao = expressao.replace(String.format("%s:", propriedade), ""); // retirar o inicion como ex:"title: o
+                                                                                  // filme", para apenas "o filme"
+            listaResultado.add(expressao);
         }
+        System.out.println(listaResultado.size());
         return listaResultado;
     }
 
-    public void mostrarTitulos()
-    {
-       List<String> listaResultado = getTitles();
+    public void mostrarTitulos() {
+        List<String> listaResultado = getTitles();
+
+        for (String item : listaResultado) {
+            System.out.println(item);
+        }
+    }
+
+    public void mostrarPosters() {
+        List<String> listaResultado = getposterPath();
+
+        for (String item : listaResultado) {
+            System.out.println(item);
+        }
+    }
+
+    public void mostrarMedias() {
+        List<String> listaResultado = getvoteAverage();
+
         for (String item : listaResultado) {
             System.out.println(item);
         }
@@ -46,6 +89,22 @@ public class Filmes {
 
     public void setTitles(List<String> titles) {
         this.titles = titles;
+    }
+
+    public List<String> getposterPath() {
+        return posterPath;
+    }
+
+    public void setposterPath(List<String> posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public List<String> getvoteAverage() {
+        return voteAverage;
+    }
+
+    public void setvoteAverage(List<String> voteAverage) {
+        this.voteAverage = voteAverage;
     }
 
 }
--- src/RankingDosTopFilmes.java
+++ src/RankingDosTopFilmes.java
@@ -4,41 +4,44 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class RankingDosTopFilmes {
     public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
 
-        // definindo variavel que vai conter no resultado
         String json = "";
+        for (int pages = 1; pages < 13; pages++) // para eu poder percorrer varias paginas e juntar tudo num json
+        {
+            // Minha key de acesso a API
+            String key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTg2YmYyNzE4MWJkMGU1ZTMzOGM0ZjQyNzg1OTNkZSIsInN1YiI6IjY2NjI1MDVkOGZmYjZiMjE0M2YyMzg3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HeHFhJGkLyWsgaNLz8Yv173ANAxFMCGtrr4kN-zSSGw";
 
-        try {
-            // Criando o cliente
-            HttpClient client = HttpClient.newHttpClient();
+            String apiURL = String.format("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR-US&page=%s&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200", pages);
 
-            // Criando a solicitação
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI("http://localhost:3000/tarefas"))
-                    .GET()
-                    .build();
+            try // GET
+            {
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(apiURL))
+                        .header("accept", "application/json")
+                        .header("Authorization",
+                                String.format("Bearer %s", key))
+                        .method("GET", HttpRequest.BodyPublishers.noBody())
+                        .build();
 
-            // Enviando a solicitação e obtendo a resposta
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                // Enviar a solicitação e receber a resposta
+                HttpResponse<String> response = HttpClient.newHttpClient().send(request,
+                        HttpResponse.BodyHandlers.ofString());
 
-            // Imprimindo o status e o corpo da resposta
+                // Colocar o valor da resposta dentro de uma variavel
+                json += response.body();
 
-            json = response.body();
-
-            // System.out.println("Response Code: " + response.statusCode());
-            // System.out.println("Response Body: " + json);
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         Filmes filminhos = new Filmes(json);
-        filminhos.mostrarTitulos();
+        //filminhos.mostrarPosters();
+        //filminhos.mostrarTitulos();
+        //filminhos.mostrarMedias();
     }
 }
