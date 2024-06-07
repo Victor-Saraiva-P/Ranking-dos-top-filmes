883d111eb8b57ed171ab158a74d79c2eba70a581
Victor Alexandre Saraiva Pimentel <victor_alex2005@hotmail.com>
2024-06-06 22:36:39 -0300

    criação da area de teste.java

---
+++ src/teste.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class teste {
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+    
+    }
+}
