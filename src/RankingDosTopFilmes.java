12ebeeac1d02d4e296c9c55e871421062d6ee60f
Victor Alexandre Saraiva Pimentel <victor_alex2005@hotmail.com>
2024-06-06 22:10:28 -0300

    area inicial de testes

--- src/RankingDosTopFilmes.java
+++ src/RankingDosTopFilmes.java
@@ -7,26 +7,21 @@ import java.net.http.HttpResponse;
 
 public class RankingDosTopFilmes {
     public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
-        // Minha key de acesso a API
-        String key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTg2YmYyNzE4MWJkMGU1ZTMzOGM0ZjQyNzg1OTNkZSIsInN1YiI6IjY2NjI1MDVkOGZmYjZiMjE0M2YyMzg3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HeHFhJGkLyWsgaNLz8Yv173ANAxFMCGtrr4kN-zSSGw";
+        try {
+            // Criando o cliente
+            HttpClient client = HttpClient.newHttpClient();
 
-        String apiURL = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200";
-
-        try // GET
-        {
+            // Criando a solicitação
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(apiURL))
-                    .header("accept", "application/json")
-                    .header("Authorization",
-                            String.format("Bearer %s", key))
-                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .uri(new URI("http://localhost:3000/tarefas"))
+                    .GET()
                     .build();
 
-            // Enviar a solicitação e receber a resposta
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            
-            // Imprimir o status e o corpo da resposta
-            System.out.println("Status Code: " + response.statusCode());
+            // Enviando a solicitação e obtendo a resposta
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Imprimindo o status e o corpo da resposta
+            System.out.println("Response Code: " + response.statusCode());
             System.out.println("Response Body: " + response.body());
         } catch (Exception e) {
             e.printStackTrace();
